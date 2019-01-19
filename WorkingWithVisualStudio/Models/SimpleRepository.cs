9c8c4e715884d712873adde6a65796fd9d1a4801
Andrew Olejnik <aolejnikccsj@outlook.com>
2019-01-19 14:30:01 -0600

    Add project files.

---
+++ WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products
+            = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var intialItem = new[]
+            {
+            new Product { Name = "Kayak", Price= 275m },
+            new Product { Name = "LifeJacket" , Price = 48.95m },
+            new Product { Name = "Soccer Ball" , Price = 19.50m },
+            new Product { Name = "Corner flag" , Price = 34.95m}
+
+        };
+
+            foreach (var p in intialItem)
+            {
+                AddProduct(p);
+
+            }
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+
+    }
+
+}
+
